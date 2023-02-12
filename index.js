195b5e0d56b588176537c026b2a895b553ff0d98
PPlain <124816397+PPlain@users.noreply.github.com>
2023-02-12 11:28:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content === '!hello') {
+        message.channel.send('Hello!');
+    }
+});
+
+client.login('MTA3NDAyNjgyMDM4MDM5NzYxOA.GXaYL9.4zHqn_Y8aLov-EhiRoNvXZrPJBAfcXqGg9IJMQ');
